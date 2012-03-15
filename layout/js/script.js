44dc5217c58ad01b5331997d5e80497ca609d3e8
Artur Kornakov <itisshortemailaddress@gmail.com>
2012-03-15 22:39:12 +1100

    Add free api key and geolocation

--- layout/js/script.js
+++ layout/js/script.js
@@ -1,18 +1,35 @@
 $(document).ready(function() {
-    var $mapContainer = $(".js-map");
+//    var $mapContainer = $(".js-map");
 
-    var map = new L.Map('map');
-    var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/YOUR-API-KEY/997/256/{z}/{x}/{y}.png', {
+    var map = new L.Map($(".js-map")[0]);
+    var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/b046d1ecb48e42b6a6566b08c1f377ac/997/256/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
         maxZoom: 18
     });
 
-    //get geolocation coordinats for user
-    var london = new L.LatLng(51.505, -0.09); // geographical point (longitude and latitude)
-    map.setView(london, 13).addLayer(cloudmade);
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function(position){
+                var longitude = position.coords.longitude,
+                    latitude = position.coords.latitude;
 
-    //set markers
-    var markerLocation = new L.LatLng(51.5, -0.09);
-    var marker = new L.Marker(markerLocation);
-    map.addLayer(marker);
+                console.log(position);
+                //get geolocation coordinats for user
+                //TODO rewrite comment in leaflet wiki on github!!!!
+                var mapCenter = new L.LatLng(latitude, longitude); // geographical point (longitude and latitude)
+                map.setView(mapCenter, 15).addLayer(cloudmade);
+
+                //set markers
+//                var youAreHere = new L.LatLng(latitude, longitude);
+                var youAreHereMarker = new L.Marker(mapCenter);
+                map.addLayer(youAreHereMarker);
+
+            },
+            //error handler
+            function (){
+
+            }
+        );
+    }
+
 });
