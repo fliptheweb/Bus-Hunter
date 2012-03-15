44ef36de391340a110c6c8c00e0eaa3787f49f0a
Artur Kornakov <itisshortemailaddress@gmail.com>
2012-03-15 18:46:30 +1100

    Add leaflet from cdn

--- layout/js/script.js
+++ layout/js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
+    var $mapContainer = $(".js-map");
 
+    var map = new L.Map('map');
+    var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/YOUR-API-KEY/997/256/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+        maxZoom: 18
+    });
+
+    //get geolocation coordinats for user
+    var london = new L.LatLng(51.505, -0.09); // geographical point (longitude and latitude)
+    map.setView(london, 13).addLayer(cloudmade);
+
+    //set markers
+    var markerLocation = new L.LatLng(51.5, -0.09);
+    var marker = new L.Marker(markerLocation);
+    map.addLayer(marker);
 });
